253a5c16d0065b09a2dde77fef4e5e57456ba533
orange.toaster <orange.toaster@gmail.com>
2018-12-02 23:03:52 -0500

    Now this is music.

--- main.c
+++ main.c
@@ -8,19 +8,20 @@ typedef int retcode;
 #define retfail(CONDITION) { retcode __retval = CONDITION ; if((__retval < 0)) \
   { fprintf(stderr, "Failed: %s\n In %s on line %d - %d\n", #CONDITION, __FILE__, __LINE__, __retval ); return __retval; } }
 
-#define TABLE_SIZE 800
+#define TABLE_SIZE 256
 typedef struct {
   kiss_fft_scalar buffer_data[TABLE_SIZE]; // pre-buffer size
   kiss_fft_cpx freq_domain[TABLE_SIZE/2]; // pre-buffer size
   int left_phase;
-  int right_phase;
+  int note_pos;
+  kiss_fftr_cfg cfg;
 } LRAudioBuf;
 
 // This can be called at interrupt level, so nothing fancy, no malloc/free
 static int audio_buffer_sync_callback(
     const void *inputBuffer, // unused
     void *outputBuffer,
-    unsigned long framesPerBuffer,
+    unsigned long framesPerBuffer, // This tends to be 64
     const PaStreamCallbackTimeInfo* timeInfo, // unused
     PaStreamCallbackFlags stats, // unused
     void *context ) {
@@ -29,18 +30,25 @@ static int audio_buffer_sync_callback(
   float *out = (float*)outputBuffer;
   unsigned long i;
 
-  audio_buf->left_phase = audio_buf->right_phase = 0;
   for( i=0; i<framesPerBuffer; i++ ) {
     *out++ = audio_buf->buffer_data[audio_buf->left_phase];  // left channel
-    *out++ = audio_buf->buffer_data[audio_buf->right_phase];  // right channel
     audio_buf->left_phase += 1;
-    if( audio_buf->left_phase >= TABLE_SIZE ) audio_buf->left_phase -= TABLE_SIZE;
-    audio_buf->right_phase += 16;
-    if( audio_buf->right_phase >= TABLE_SIZE ) audio_buf->right_phase -= TABLE_SIZE;
+    if( audio_buf->left_phase >= TABLE_SIZE ) {
+      audio_buf->left_phase -= TABLE_SIZE;
+      audio_buf->freq_domain[audio_buf->note_pos].i = 0;
+      audio_buf->note_pos = (audio_buf->note_pos + 1) % (TABLE_SIZE/2);
+      audio_buf->freq_domain[audio_buf->note_pos].i = -200;
+      kiss_fftri(audio_buf->cfg, audio_buf->freq_domain, audio_buf->buffer_data);
+    }
   }
   return paContinue;
 }
 
+static void cleanup(void *context) {
+  LRAudioBuf *audio_buf = (LRAudioBuf*)context;
+  free(audio_buf->cfg);
+}
+  
 int near(float a, float b, float epsilon) {
   return (a + epsilon > b && a - epsilon < b);
 }
@@ -52,28 +60,17 @@ static int init_portaudio(PaStream **stream) {
   
   // init the buzz with a real ifft
   memset(&audio_buf, 0, sizeof(audio_buf));
-  audio_buf.freq_domain[1].i = -200;
-  audio_buf.freq_domain[3].i = 100;
-
-  kiss_fftr_cfg cfg = kiss_fftr_alloc(TABLE_SIZE, 1, NULL,NULL);
-  kiss_fftri(cfg, audio_buf.freq_domain, audio_buf.buffer_data);
-  free(cfg);
-
-  for(int i=0; i<TABLE_SIZE/2; ++i){
-    if (!near(audio_buf.freq_domain[i].i, 0.0, 1e-4)) {
-      printf("%d: %.4f ", i, audio_buf.freq_domain[i].i);
-    }
-  }
+  audio_buf.cfg = kiss_fftr_alloc(TABLE_SIZE, 1, NULL,NULL);
 
   retfail(Pa_Initialize());
 
   outputParameters.device = Pa_GetDefaultOutputDevice(); /* default output device */
   retfail(outputParameters.device == paNoDevice) // no default output device
   
-  outputParameters.channelCount = 2;       /* stereo output */
+  outputParameters.channelCount = 1; // If this is stereo, then the autputBuffer is (L, R) tuples
   outputParameters.sampleFormat = paFloat32; /* 32 bit floating point output */
   outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
-  //outputParameters.hostApiSpecificStreamInfo = NULL;
+  outputParameters.hostApiSpecificStreamInfo = NULL;
 
   retfail(Pa_OpenStream(
       stream,
@@ -84,6 +81,8 @@ static int init_portaudio(PaStream **stream) {
       paClipOff,
       audio_buffer_sync_callback,
       &audio_buf )); // this is context in the callback
+  
+  retfail(Pa_SetStreamFinishedCallback(*stream, &cleanup));
 
   return 0;
 }
@@ -102,7 +101,7 @@ int main() {
   PaStream *stream = NULL;
   printf("Hello sound\n");
   retfail(init_portaudio(&stream));
-  retfail(play_sound(stream, .5));
+  retfail(play_sound(stream, 2));
   Pa_Terminate();
 
   printf("All good.\n");
